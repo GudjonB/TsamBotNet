496efccea6047edbefd19b1eb7eb51b29c162ea9
Gudjon <gudjon17@ru.com>
2019-10-13 00:31:37 +0000

    newline check

--- client.cpp
+++ client.cpp
@@ -113,8 +113,7 @@ int main(int argc, char *argv[])
     {
         bzero(buffer, sizeof(buffer));
         fgets(buffer, sizeof(buffer) - 1, stdin);
-        std::cout << "strlen : " << strlen(buffer) << std::endl;
-        if (strlen(buffer) != 0){
+        if (*buffer != '\n'){
             nwrite = send(serverSocket, buffer, strlen(buffer), 0);
 
             if (nwrite == -1)
